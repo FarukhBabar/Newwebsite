03283df7180b59b54f4838a9187dc5d777f77298
FarukhBabar <fahadawan32085346@gmail.com>
2024-09-20 17:27:57 +0500

    friday

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -35,8 +35,8 @@ const Footer = () => {
                 <h6 className="text-uppercase font-weight-bold" style={{ color: '#f5f5f5' }}>Contact</h6>
                 <p><i className="fas fa-home mr-3" /> New York, NY 10012, US</p>
                 <p><i className="fas fa-envelope mr-3" /> info@example.com</p>
-                <p><i className="fas fa-phone mr-3" /> +1 234 567 88</p>
-                <p><i className="fas fa-print mr-3" /> +1 234 567 89</p>
+                <a href='#'><i className="fas fa-phone mr-3" /> +1 234 567 88</a>
+                <a href='#'><i className="fas fa-print mr-3" /> +1 234 567 89</a>
               </div>
 
               {/* Social Media Column */}
